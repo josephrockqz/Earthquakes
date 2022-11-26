39aad3fdfcf2a0b3bf20fc6221001dc8a4a7c4b9
Joseph Rock <josephrockqz@gmail.com>
2022-11-26 18:13:02 -0500

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Joseph Rock on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
