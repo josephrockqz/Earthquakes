b272a004f411be9e504c7d9f4e68b10d113f6e81
Joseph Rock <josephrockqz@gmail.com>
2022-11-30 20:11:54 -0500

    Built a network test client

---
+++ Earthquakes/Preview Content/TestDownloader.swift
@@ -0,0 +1,16 @@
+//
+//  TestDownloader.swift
+//  Earthquakes
+//
+//  Created by Joseph Rock on 11/30/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+
+class TestDownloader: HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+        try await Task.sleep(nanoseconds: UInt64.random(in: 100_000_000...500_000_000))
+        return testQuakesData
+    }
+}
