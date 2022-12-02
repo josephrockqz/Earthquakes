8599a699252114c4d4bb7ae6e5fa2850d0124ce8
Joseph Rock <josephrockqz@gmail.com>
2022-12-01 22:22:03 -0500

    Added detail view

--- Earthquakes/Models/QuakesProvider.swift
+++ Earthquakes/Models/QuakesProvider.swift
@@ -10,11 +10,11 @@ import Foundation
 
 @MainActor
 class QuakesProvider: ObservableObject {
-    
+
     @Published var quakes: [Quake] = []
-    
-    let client: QuakeClient
-    
+
+    private let client: QuakeClient
+
     func fetchQuakes() async throws {
         let latestQuakes = try await client.quakes
         self.quakes = latestQuakes
@@ -23,7 +23,11 @@ class QuakesProvider: ObservableObject {
     func deleteQuakes(atOffsets offsets: IndexSet) {
         quakes.remove(atOffsets: offsets)
     }
-    
+
+    func location(for quake: Quake) async throws -> QuakeLocation {
+        return try await client.quakeLocation(from: quake.detail)
+    }
+
     init(client: QuakeClient = QuakeClient()) {
         self.client = client
     }
